51110f84d10a0d5457ba75f6567d82c9a143e7d5
Thomas Dhaussy <94963203+DHS5@users.noreply.github.com>
2024-04-24 14:03:11 +0200

    Ball

--- src/Game/Ball.cpp
+++ src/Game/Ball.cpp
@@ -3,3 +3,15 @@
 //
 
 #include "Ball.h"
+
+void Ball::Update()
+{
+	//shape.move(m_speed * m_direction);
+}
+
+void Ball::Collide(sf::Vector2f collisionNormal)
+{
+	float x = collisionNormal.x != 0.0 ? m_direction.x * collisionNormal.x : m_direction.x;
+	float y = collisionNormal.y != 0.0 ? m_direction.y * collisionNormal.y : m_direction.y;
+	m_direction = sf::Vector2f(x, y);
+}
--- src/Game/Ball.h
+++ src/Game/Ball.h
@@ -5,8 +5,17 @@
 #ifndef CMAKESFMLPROJECT_BALL_H
 #define CMAKESFMLPROJECT_BALL_H
 
+#include "SFML/Graphics.hpp";
 
 class Ball {
+
+public:
+	void Update();
+	void Collide(sf::Vector2f collisionNormal);
+
+private:
+	float m_speed;
+	sf::Vector2f m_direction;
 
 };
 
