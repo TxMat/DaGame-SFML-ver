299a53f4d8d9c7f2cce6bb2cb6db136526ba6d43
Custromix <75646944+Custromix@users.noreply.github.com>
2024-05-15 11:57:10 +0200

    clean

--- src/main.cpp
+++ src/main.cpp
@@ -1,48 +1,13 @@
 #include "SFML/Graphics.hpp"
 #include "Game/MainScene.h"
-#include <iostream>
 
 #include "App.h"
-#include "Network/UDPCommunicator.h"
 #include "Common/Globals.h"
 
 int main() {
 
-    UDPCommunicator udpComm;
-
-    // Initialisation du socket
-    if (!udpComm.initSocket()) {
-        return -1;
-    }
-
-    // Liaison du socket au port 8080
-    if (!udpComm.bindSocket(8080)) {
-        return -1;
-    }
-
-    // Envoi d'un message
-    if (udpComm.sendMessage("Bonjour", "127.0.0.1", 8080)) {
-        //std::count << "Message envoyé: Bonjour, UDP!" << std::endl;
-    }
-
-
-    while (true) {
-        std::string receivedMessage;
-        if (udpComm.receiveMessage(receivedMessage)) {
-            std::cout << "Message reçu: TTTT " << receivedMessage << std::endl;
-        }
-        else {
-            std::cerr << "Erreur lors de la réception du message" << std::endl;
-        }
-    }
-
-    
-
-    return 0;
-
-    
-    /*auto app = App();
+    auto app = App();
     app.Init();
-    app.MainLoop();*/
+    app.MainLoop();
     
 }
