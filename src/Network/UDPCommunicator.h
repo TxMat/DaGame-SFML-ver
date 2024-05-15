0c0636982c46e604d2ba36d6f6e62e7c293ecdaf
Custromix <75646944+Custromix@users.noreply.github.com>
2024-05-15 11:22:46 +0200

    Update UDPCommunicator.h

--- src/Network/UDPCommunicator.h
+++ src/Network/UDPCommunicator.h
@@ -1,32 +1,87 @@
-#ifndef UDPCOMMUNICATOR_H
-#define UDPCOMMUNICATOR_H
+#include "UDPCommunicator.h"
+#include <iostream>
+#include <cstring>
 
-#include <string>
-
-// Définition des includes et des types pour Windows et Unix/Linux
 #ifdef _WIN32
-#include <winsock2.h>
-#include <ws2tcpip.h>
-typedef SOCKET socket_t;
-typedef int socklen_t;
-#else
-#include <arpa/inet.h>
-#include <unistd.h>
-typedef int socket_t;
+#pragma comment(lib, "ws2_32.lib")
 #endif
 
-class UDPCommunicator {
-public:
-    UDPCommunicator();
-    ~UDPCommunicator();
+UDPCommunicator::UDPCommunicator() : sockfd(-1) {
+#ifdef _WIN32
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        std::cerr << "Erreur lors de l'initialisation de Winsock" << std::endl;
+    }
+#endif
+}
 
-    bool initSocket();
-    bool bindSocket(int port);
-    bool sendMessage(const std::string& message, const std::string& address, int port);
-    bool receiveMessage(std::string& message, int bufferSize = 1024);
+UDPCommunicator::~UDPCommunicator() {
+    if (sockfd != -1) {
+#ifdef _WIN32
+        closesocket(sockfd);
+        WSACleanup();
+#else
+        close(sockfd);
+#endif
+    }
+}
 
-private:
-    socket_t sockfd;
-};
+bool UDPCommunicator::initSocket() {
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sockfd < 0) {
+        std::cerr << "Erreur lors de la création du socket" << std::endl;
+        return false;
+    }
+    return true;
+}
 
-#endif // UDPCOMMUNICATOR_H
+bool UDPCommunicator::bindSocket(int port) {
+    sockaddr_in servaddr;
+    std::memset(&servaddr, 0, sizeof(servaddr));
+
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = INADDR_ANY;
+    servaddr.sin_port = htons(port);
+
+    if (bind(sockfd, (const struct sockaddr*)&servaddr, sizeof(servaddr)) < 0) {
+        std::cerr << "Erreur lors de la liaison du socket" << std::endl;
+#ifdef _WIN32
+        closesocket(sockfd);
+#else
+        close(sockfd);
+#endif
+        return false;
+    }
+    return true;
+}
+
+bool UDPCommunicator::sendMessage(const std::string& message, const std::string& address, int port) {
+    sockaddr_in servaddr;
+    std::memset(&servaddr, 0, sizeof(servaddr));
+
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(port);
+    servaddr.sin_addr.s_addr = inet_addr(address.c_str());
+
+    if (sendto(sockfd, message.c_str(), message.length(), 0, (const struct sockaddr*)&servaddr, sizeof(servaddr)) < 0) {
+        std::cerr << "Erreur lors de l'envoi du message" << std::endl;
+        return false;
+    }
+    return true;
+}
+
+bool UDPCommunicator::receiveMessage(std::string& message, int bufferSize) {
+    char buffer[bufferSize];
+    sockaddr_in cliaddr;
+    socklen_t len = sizeof(cliaddr);
+
+    int n = recvfrom(sockfd, buffer, bufferSize - 1, 0, (struct sockaddr*)&cliaddr, &len);
+    if (n < 0) {
+        std::cerr << "Erreur lors de la réception du message" << std::endl;
+        return false;
+    }
+
+    buffer[n] = '\0';
+    message = std::string(buffer);
+    return true;
+}
