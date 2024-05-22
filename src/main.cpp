7bf7103f75d1b187dda5e217252afa2eddd625e0
Thomas Dhaussy <94963203+DHS5@users.noreply.github.com>
2024-05-22 14:19:57 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,15 @@
 
 #include "App.h"
 #include "Common/Globals.h"
+#include "Network/MainNetwork.h"
 
 int main() {
 
-    auto app = App();
-    app.Init();
-    app.MainLoop();
+    auto net = MainNetwork();
+    net.start();
+
+    //auto app = App();
+    //app.Init();
+    //app.MainLoop();
     
 }
