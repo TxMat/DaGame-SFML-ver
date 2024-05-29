749f484b0651b46906acbc702cbc9f11c1cb8a9d
Mathieu Ponal <mathieu.ponal@gmail.com>
2024-05-29 16:03:27 +0200

    one byte

--- src/Common/Systems/PacketType.h
+++ src/Common/Systems/PacketType.h
@@ -5,10 +5,10 @@
 #ifndef CMAKESFMLPROJECT_PACKETTYPE_H
 #define CMAKESFMLPROJECT_PACKETTYPE_H
 
-enum PacketType {
-    Conn,
-    ConnAck,
-    PosSync,
+enum PacketType : char {
+    Conn = 0,
+    ConnAck = 1,
+    PosSync = 2,
 };
 
 #endif //CMAKESFMLPROJECT_PACKETTYPE_H
